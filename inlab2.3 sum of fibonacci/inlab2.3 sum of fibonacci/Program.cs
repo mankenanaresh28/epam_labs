47bce9c1839bcd00436dae182623e0b31031d6e1
Mankena Naresh <113589641+mankenanaresh28@users.noreply.github.com>
2024-03-29 10:16:27 +0530

    Add files via upload

---
+++ inlab2.3 sum of fibonacci/inlab2.3 sum of fibonacci/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace FibonacciSum;
+class FibonacciSum
+{
+    public static void Main(string[] args)
+    {
+        int a = Convert.ToInt32(Console.ReadLine());
+        int f0 = 0, f1 = 1,s = f1+f0,f2=0;
+        for(int i=2;i<a;i++)
+        {
+            f2 = f0 + f1;
+            s = f2 + s;
+            f0 = f1;
+            f1 = f2;
+        }
+        Console.WriteLine(s);
+
+
+    }
+}
