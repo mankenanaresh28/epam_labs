9004f12c5c86b47d8cb7b771b73bbe97706bc41a
Mankena Naresh <113589641+mankenanaresh28@users.noreply.github.com>
2024-03-29 10:14:16 +0530

    Add files via upload

---
+++ lab6 post/lab6 post/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public static class MathExtensions
+{
+    // Extension method to calculate the sum of digits of an integer
+    public static int SumDigits(this int number)
+    {
+        int sum = 0;
+        while (number != 0)
+        {
+            sum += number % 10;
+            number /= 10;
+        }
+        return sum;
+    }
+
+    // Extension method to reverse an integer
+    public static int Reverse(this int number)
+    {
+        int reverse = 0;
+        while (number != 0)
+        {
+            reverse = reverse * 10 + number % 10;
+            number /= 10;
+        }
+        return reverse;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number = 1234;
+        int sumOfDigits = number.SumDigits(); // 1 + 2 + 3 + 4 = 10
+        int reversedNumber = number.Reverse(); // 4321
+
+        Console.WriteLine($"Sum of digits of {number}: {sumOfDigits}");
+        Console.WriteLine($"Reverse of {number}: {reversedNumber}");
+    }
+}
